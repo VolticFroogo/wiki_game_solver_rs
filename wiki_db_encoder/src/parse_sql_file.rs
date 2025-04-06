a113cc67da7e6950aa6cb9ed0cfabdf096c403b7
Harry <harry@froogo.co.uk>
2025-04-07 00:50:33 +0100

    Refactor

--- wiki_db_encoder/src/parse_sql_file.rs
+++ wiki_db_encoder/src/parse_sql_file.rs
@@ -10,32 +10,19 @@ pub fn parse_sql_file<F: FnMut(&Captures)>(file_name: &str, regex: Regex, mut fu
     let start = Instant::now();
 
     let mut file = File::open(file_name)?;
+    let file_size = file.metadata()?.len() as usize;
     let mut buffer = [0u8; CHUNK_SIZE];
-
-    let bytes_read = file.read(&mut buffer)?;
-    let bytes_as_string = String::from_utf8_lossy(&buffer[..bytes_read]);
-    if let Some(pos) = bytes_as_string.find("VALUES") {
-        let offset_pos = pos + "VALUES ".len();
-        file.seek(SeekFrom::Start(offset_pos as u64))?;
-    } else {
-        return Err("Could not find 'VALUES' in the file".into());
-    }
-
     let mut total_bytes_read = 0usize;
-    let total_file_size = file.metadata()?.len() as usize;
+
+    file_seek_to_values(&mut file, &mut buffer)?;
+
     while let Ok(bytes_read) = file.read(&mut buffer) {
         if bytes_read == 0 {
             break; // EOF
         }
 
         total_bytes_read += bytes_read;
-        if total_bytes_read % (1024 * CHUNK_SIZE) == 0 {
-            println!("Read {}/{} MiB ({}%) of {}",
-                     total_bytes_read / (1024 * 1024),
-                     total_file_size / (1024 * 1024),
-                     (total_bytes_read * 100) / total_file_size,
-                     file_name);
-        }
+        print_progress(file_name, file_size, &total_bytes_read);
 
         let bytes_as_string = String::from_utf8_lossy(&buffer[..bytes_read]);
         let captures: Vec<Captures> = regex.captures_iter(&bytes_as_string).collect();
@@ -45,13 +32,43 @@ pub fn parse_sql_file<F: FnMut(&Captures)>(file_name: &str, regex: Regex, mut fu
             break; // EOF
         }
 
-        if let Some(cap) = captures.last() {
-            let last = cap.iter().last().unwrap().unwrap();
-            let offset = last.end() as i64 - CHUNK_SIZE as i64 + 3;
-            file.seek_relative(offset)?;
-        }
+        file_seek_to_end_of_last_match(&mut file, captures.last())?;
     }
 
     println!("Finished reading {} in {}s", file_name, start.elapsed().as_secs());
     Ok(())
+}
+
+fn file_seek_to_values(file: &mut File, mut buffer: &[u8; CHUNK_SIZE]) -> Result<(> {
+    let bytes_read = file.read(&mut buffer)?;
+    let bytes_as_string = String::from_utf8_lossy(&buffer[..bytes_read]);
+
+    if let Some(pos) = bytes_as_string.find("VALUES") {
+        let offset_pos = pos + "VALUES ".len();
+        file.seek(SeekFrom::Start(offset_pos as u64))?;
+    } else {
+        return Err("Could not find 'VALUES' in the file".into());
+    }
+
+    Ok(())
+}
+
+fn file_seek_to_end_of_last_match(file: &mut File, last_capture: Option<&Captures>) -> Result<()> {
+    if let Some(cap) = last_capture {
+        let last = cap.iter().last().unwrap().unwrap();
+        let offset = last.end() as i64 - CHUNK_SIZE as i64;
+        file.seek_relative(offset)?;
+    }
+
+    Ok(())
+}
+
+fn print_progress(file_name: &str, file_size: usize, total_bytes_read: &usize) {
+    if total_bytes_read % (1024 * CHUNK_SIZE) == 0 {
+        println!("Read {}/{} MiB ({}%) of {}",
+                 total_bytes_read / (1024 * 1024),
+                 file_size / (1024 * 1024),
+                 (total_bytes_read * 100) / file_size,
+                 file_name);
+    }
 }
