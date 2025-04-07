040d33bfea9f3b0e8b5cba47ed9d00cf65bec188
Harry <harry@froogo.co.uk>
2025-04-07 13:42:38 +0100

    Add web endpoint

--- wiki_game_solver_web/src/main.rs
+++ wiki_game_solver_web/src/main.rs
@@ -1,27 +1,65 @@
 mod bfs;
 
 use std::error::Error;
+use std::sync::Arc;
 use std::time::Instant;
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use actix_web::http::header::ContentType;
+use actix_web::web::Data;
+use serde::{Deserialize, Serialize};
+use tokio::sync::RwLock;
+use crate::bfs::LinkData;
 
 pub type Result<T> = std::result::Result<T, Box<dyn Error + Send + Sync>>;
 
-fn main() -> Result<()> {
-    let link_data = bfs::get_link_data()?;
+#[derive(Deserialize)]
+struct Query {
+    start: u32,
+    target: u32,
+}
 
-    let start = 6912103;
-    let target = 45567357;
-    // let start = 62877952;
-    // let target = 29086544;
+#[derive(Serialize)]
+struct Response {
+    path: Vec<u32>,
+    seconds_taken: f64,
+}
+
+#[get("/solve")]
+async fn solve(link_data: web::Data<Arc<RwLock<LinkData>>>, query: web::Query<Query>) -> impl Responder {
+    let link_data = link_data.read().await;
 
     let start_time = Instant::now();
-    let path = bfs::bfs_bidirectional(&link_data, start, target);
+
+    let path = bfs::bfs_bidirectional(link_data, query.start, query.target);
     if path.is_none() {
         println!("Path determined unreachable in {}s", start_time.elapsed().as_secs_f64());
-        return Ok(());
+        return HttpResponse::UnprocessableEntity().body("abc");
     }
+    let path = path.unwrap();
 
-    println!("Found path in {}s", start_time.elapsed().as_secs_f64());
-    println!("Path: {}", path.unwrap().iter().map(|x| x.to_string()).collect::<Vec<String>>().join("|"));
+    let seconds_taken = start_time.elapsed().as_secs_f64();
+    println!("Found {} degree path from {} to {} in {}s", path.len()-1, query.start, query.target, seconds_taken);
+
+    HttpResponse::Ok()
+        .content_type(ContentType::json())
+        .body(serde_json::to_string(&Response{
+            path,
+            seconds_taken,
+        }).unwrap())
+}
+
+#[actix_web::main]
+async fn main() -> Result<()> {
+    let link_data = Arc::new(RwLock::new(bfs::get_link_data()?));
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(Data::new(link_data.clone()))
+            .service(solve)
+    })
+    .bind(("0.0.0.0", 8080))?
+    .run()
+    .await?;
 
     Ok(())
 }
